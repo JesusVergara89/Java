e42418bd7e6aa058a078523a0a89857da1ea4869
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-15 22:48:19 -0700

    default values

---
+++ Defaultvalues.java
@@ -0,0 +1,13 @@
+public class Defaultvalues {
+    public static void main(String[] args) {
+        // byte = 0
+        // short = 0
+        // int = 0
+        // long = 0
+        // float = 0.0f
+        // double = 0.0d
+        // boolean = false
+        // char = '\u0000'
+        // string = null
+    }
+}
