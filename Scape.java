a45a043ffc313de23bba0998fdac391acb3f2df1
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-15 22:43:09 -0700

    scape and string files

---
+++ Scape.java
@@ -0,0 +1,6 @@
+public class Scape {
+    public static void main(String[] args) {
+        String text = "\n\b\t\u2605 this is an string\n this is another string";
+        System.out.println(text);
+    }
+}
---
+++ Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    public static void main(String[] args) {
+        String text = "lorem ipsum";
+
+        int long_string = text.length();
+
+        System.out.println(long_string);
+    }
+}
