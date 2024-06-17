6fdef52b6c06c43f67f79ad2dccf44301416b10e
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-16 18:21:49 -0700

    math finished

--- Mathoperators.java
+++ Mathoperators.java
@@ -1,3 +1,15 @@
 public class Mathoperators {
-    
+    public static void main(String[] args) {
+
+        int m =5, n = 5;
+        double radio = (int)(Math.random()*10);
+        double result = Math.pow(m, n);
+        double square_result = Math.sqrt(result);
+        double area = Math.PI*radio*radio;
+        System.out.println(result);
+        System.out.println(square_result);
+        System.out.println(area);
+        System.out.println(radio);
+
+    }
 }
