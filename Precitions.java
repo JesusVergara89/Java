4fef55a23cdd7b07a843022ef147e4ec6bdd54a4
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-16 18:30:44 -0700

    precitions in java

---
+++ Precitions.java
@@ -0,0 +1,14 @@
+public class Precitions {
+    public static void main(String[] args) {
+        double a = 0.1 + 0.2;
+        double b = 0.1f + 0.2f;
+        float c = 0.1f + 0.2f;
+
+        double d = 10d/3d;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
