7ca24bf28f699a50125ac08f56cfe8267bb5c52a
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-17 09:18:20 -0700

    conditions and logic ope

--- Conditions.java
+++ Conditions.java
@@ -1,5 +1,12 @@
 public class Conditions {
     public static void main(String[] args) {
-        
+        int x = 0, y = 3;
+
+        if (x == 1)
+            System.out.println("x is equal to 1");
+        else if (y == 2)
+            System.out.println("y is  equal to 2");
+        else
+            System.out.println("x and y are not equal to the values");
     }
 }
---
+++ Logicope.java
@@ -0,0 +1,13 @@
+public class Logicope {
+    public static void main(String[] args) {
+        boolean isFalse = false;
+        boolean value1 = true;
+        boolean value2 = false;
+
+        boolean example = value1 && value2;
+
+
+        System.out.println(!isFalse);
+        System.out.println(example);
+    }
+}
