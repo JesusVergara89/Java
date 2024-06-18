fdcb97d9be060229e89027c23de64c42b6a1511a
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-18 12:03:35 -0700

    this is switch

--- Switch.java
+++ Switch.java
@@ -1,5 +1,14 @@
 public class Switch {
     public static void main(String[] args) {
-        
+        int x = 3;
+
+        switch (x)  {
+            case 1:
+            System.out.println("El valor no es 1");break;
+            case 3:
+            System.out.println("El valor es 3");break;        
+            default:
+            System.out.println("el valor no es ningun");
+        }
     }
 }
