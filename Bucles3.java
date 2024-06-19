ef234fb06ecba60351142de87090001c37fc7d32
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-19 09:28:03 -0700

    trying with for

--- Bucles3.java
+++ Bucles3.java
@@ -1,8 +1,12 @@
 public class Bucles3 {
     public static void main(String[] args) {
-        int x = 10;
+        int x = 5;
         for ( int i = 0; i < x; i++){
             System.out.println("the value of i is: " + i);
+        }
+
+        for (int j = 1, i = 5; j< i; j+=2, i++){
+            System.out.println(j);
         }
     }
 }
