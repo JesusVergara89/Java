b7990b8888fe4f01645bfc1350f6e069dd8646e2
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-19 10:03:41 -0700

    inside bucles

---
+++ Bucles5.java
@@ -0,0 +1,13 @@
+// bucles for anidados
+
+public class Bucles5 {
+    public static void main(String[] args) {
+        int a = 10, b= 4;
+
+        for ( int i = 0; i <a ; i++){
+             for (int j = 0; j < b; j++){
+                System.out.println(j);
+             }
+        }
+    }
+}
