98b2f763c77370125a58ce3def9eadb6581ee9b0
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-19 09:14:49 -0700

    bucle for

---
+++ Bucles3.java
@@ -0,0 +1,8 @@
+public class Bucles3 {
+    public static void main(String[] args) {
+        int x = 10;
+        for ( int i = 0; i < x; i++){
+            System.out.println("the value of i is: " + i);
+        }
+    }
+}
