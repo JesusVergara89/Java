cfcabb6b7b031e938894beea9cbdc3cd2f9bfed8
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-19 09:06:06 -0700

    blucle do while

--- Bucles2.java
+++ Bucles2.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
-public class Bubles1 {
+public class Bucles2 {
     public static void main(String[] args) {
         Scanner keyword = new Scanner(System.in);
         int num = 0;
-        System.out.println("Type a number disting of 0");
-        num = keyword.nextInt();
-        while (num != 0) {
+        int total = 0;
+        do {
             System.out.println("Type a number disting of 0");
             num = keyword.nextInt();
-        }
+            total += num;
+        } while ( num != 0);
+        System.out.println("The total sum is: "+total);
         keyword.close();
     }
 }
