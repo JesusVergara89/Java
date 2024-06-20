f8c78c89c67f82c8716157bfb6439b08df11443d
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-20 10:20:00 -0700

    Code block and scope of the variables

---
+++ Codeblocks.java
@@ -0,0 +1,12 @@
+public class Codeblocks {
+    public static void main(String[] args) {
+        {
+            int a = 1, b = 2, c = 3;
+            {
+                int d = 4;
+            }
+
+            //System.out.println(d); this cannot be print
+        }
+    }
+}
