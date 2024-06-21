6477b752212b3139114c127ee5b98ff3613ec6a5
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-21 10:16:23 -0700

    exc 7

---
+++ Excercises7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Excercises7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String word = input.next();
+        String aux = "";
+
+         for (int i = 0; i < word.length(); i++){
+            aux += word.charAt(i) + " "; 
+         }
+
+        System.out.println(aux);
+
+        input.close();
+    }
+}
